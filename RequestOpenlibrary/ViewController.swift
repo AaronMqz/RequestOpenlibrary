23154392cb85565114542873ceb9fcc1e5e4b582
Aarón Márquez <aaronmqz.mx@gmail.com>
2015-12-02 23:59:05 -0600

    tarea 2
    Procesando el resultado de la petición al servidor openlibrary.org

--- RequestOpenlibrary/ViewController.swift
+++ RequestOpenlibrary/ViewController.swift
@@ -12,11 +12,15 @@ class ViewController: UIViewController , UITextFieldDelegate{
 
     @IBOutlet var txtISBNSearch: UITextField!
     @IBOutlet var resultISBNSearch: UITextView!
+    @IBOutlet var lblTitulo: UILabel!
+    @IBOutlet var imgPortada: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         txtISBNSearch.delegate = self
+        lblTitulo.hidden = true
         resultISBNSearch.hidden = true
     }
 
@@ -39,13 +43,52 @@ class ViewController: UIViewController , UITextFieldDelegate{
 
             dispatch_async(dispatch_get_main_queue(), {
                 if((response) != nil){
-                   let valores = NSString(data: data!, encoding: NSUTF8StringEncoding)! as String
+                   
+                    let valores = NSString(data: data!, encoding: NSUTF8StringEncoding)! as String
                     if valores.containsString(isbnText) {
-                        self.resultISBNSearch.hidden = false
-                        self.resultISBNSearch.text = valores
+                    
+                    
+                    do{
+                        let jsonDatos = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                        let keyJsonData : String = "ISBN:" + isbnText
+                        
+                        
+                        if let datos = jsonDatos[keyJsonData] as? NSDictionary{
+                            
+                            if let nombreTitulo = datos["title"] as? String{
+                                self.lblTitulo.text = nombreTitulo
+                                self.lblTitulo.hidden = false
+                            }
+                            
+                            if let autores = datos["authors"] as? NSArray{
+                                var index: Int = 0
+                                for nombreAutor in autores {
+                                    if index == autores.count - 1 {
+                                        self.resultISBNSearch.text = self.resultISBNSearch.text + (nombreAutor["name"] as! String)
+                                    }else{
+                                        self.resultISBNSearch.text = self.resultISBNSearch.text + (nombreAutor["name"] as! String) + ", "
+                                    }
+                                    ++index
+                                }
+                                self.resultISBNSearch.hidden = false
+                            }
+
+                            if let imagen = datos["cover"] as? NSDictionary{
+                                 dispatch_async(dispatch_get_main_queue(), {
+                                    let url = NSURL(string: imagen["large"] as! String)
+                                    let data = NSData(contentsOfURL: url!)
+                                    self.imgPortada.image = UIImage(data: data!)
+                                })
+                            }
+                        }
+                    }catch _ {
+                        
+                    }
+                        
                     }else{
                         self.alert("No se econtraron resultados del ISBN introducido")
                     }
+                    
                 }else{
                     self.alert("Error al obtener los datos, revisar conexión a Internet")
                 }
@@ -55,15 +98,14 @@ class ViewController: UIViewController , UITextFieldDelegate{
         task.resume()
     }
     
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        
         if textField.text != "" {
             requestOpenlibrary(textField.text!)
             textField.resignFirstResponder()
         }else{
             alert("Introduce un ISBN")
         }
-        
         return true
     }
     
@@ -74,6 +116,7 @@ class ViewController: UIViewController , UITextFieldDelegate{
         alertController.addAction(ok)
         presentViewController(alertController, animated: true, completion: nil)
     }
+    
     
     
 }
